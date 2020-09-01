0b9fb9def847807c33d4057bde61cddffd21dcf5
Ognjen Popovic <rentorious@gmail.com>
2020-09-01 21:46:38 +0200

    clion project

---
+++ cmake-build-debug/CMakeFiles/NeonBoids.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeonBoids.dir/main.cpp.o"
+  "NeonBoids"
+  "NeonBoids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeonBoids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
